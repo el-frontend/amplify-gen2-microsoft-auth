6a9421fe6b8368443c3744335f6c5fa0b630394f
Carlos Chao <chao@factory.generalsoftwareinc.com>
2024-07-10 15:21:27 -0400

    feat: add microsoft entra

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
+import { Amplify } from "aws-amplify";
+import { signInWithRedirect } from "aws-amplify/auth";
+import { generateClient } from "aws-amplify/data";
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
+import outputs from '../amplify_outputs.json';
+
 
 const client = generateClient<Schema>();
+Amplify.configure(outputs);
 
 function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+
+  const signIn = async () => {
+    console.log("Sign in");
+    await signInWithRedirect({
+      provider: {
+        custom: "MicrosoftEntraIDSAML",
+      },
+    });
+  };
 
   useEffect(() => {
     client.models.Todo.observeQuery().subscribe({
@@ -20,6 +34,7 @@ function App() {
   return (
     <main>
       <h1>My todos</h1>
+      <button onClick={signIn}>Sign in</button>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
